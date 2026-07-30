4b63119dd9bf08eae042a1cf0927e081284f0787
b1atov <barigalovec@gmail.com>
2026-07-30 13:37:16 +0300

    fix(handlers): fix start message handler

--- handlers/message_handlers.py
+++ handlers/message_handlers.py
@@ -1,17 +1,41 @@
 from main import bot, dp
 from aiogram import types
 from keyboards.start_kb import get_start_keyboards
+from keyboards.other import subscribe_kb
+from utils.check_subscribe import check_subscribe
 import os
 
+@dp.message_handler()
+async def main_handler(message: types.Message):
+    text = message.text
+
+    if text == "Начать": 
+        await start_message(message)
+
+    elif text == "Узнать ID профиля":
+        text = (
+            f"🔘 Ваш ID Telegram профиля: <code>{message.from_user.id}</code>\n\n"
+            f"ℹ️ Данный идентификатор используется при восстановлении пароля от игрового аккаунта."
+        )
+        await message.answer(text)
+
+    elif text == "Дополнительные рассылки":
+        ...
 
 @dp.message_handler(commands=["start"])
 async def start_message(message: types.Message):
+    await message.delete()
     reply_kb, inline_kb = await get_start_keyboards()
 
-    text = f"""С помощью данного бота Вы сможете обезопасить свой игровой аккаунт от взлома и восстановить его в случае утраты пароля.
-            Для привязки игрового аккаунта, воспользуйтесь кнопкой «Получить код»
-            Перед началом взаимодействия, необходимо подписаться на наш новостной канал @{os.getenv("channel_username")}.
-    """
+    is_subscribed = await check_subscribe(message.from_user.id)
+    if not is_subscribed:
+        inline_kb = await subscribe_kb()
+
+    text = (
+        "С помощью данного бота Вы сможете обезопасить свой игровой аккаунт от взлома и восстановить его в случае утраты пароля."
+        "Для привязки игрового аккаунта, воспользуйтесь кнопкой «Получить код»"
+        f"Перед началом взаимодействия, необходимо подписаться на наш новостной канал @{os.getenv("channel_username")}."
+    )
 
     await bot.send_message(message.chat.id, "👋", reply_markup=reply_kb)
     await bot.send_message(message.chat.id, text, reply_markup=inline_kb)
