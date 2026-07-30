8dcea53e1f34cf431fbfe417af6756c54c150903
b1atov <barigalovec@gmail.com>
2026-07-30 14:20:11 +0300

    fix(handlers): fix start command handler

--- handlers/message_handlers.py
+++ handlers/message_handlers.py
@@ -5,6 +5,24 @@ from keyboards.other import subscribe_kb
 from utils.check_subscribe import check_subscribe
 import os
 
+@dp.message_handler(commands=["start"])
+async def start_message(message: types.Message):
+    await message.delete()
+    reply_kb, inline_kb = await get_start_keyboards()
+
+    is_subscribed = await check_subscribe(message.from_user.id)
+    if not is_subscribed:
+        inline_kb = await subscribe_kb()
+
+    text = (
+        "С помощью данного бота Вы сможете обезопасить свой игровой аккаунт от взлома и восстановить его в случае утраты пароля."
+        "Для привязки игрового аккаунта, воспользуйтесь кнопкой «Получить код»"
+        f"Перед началом взаимодействия, необходимо подписаться на наш новостной канал @{os.getenv("channel_username")}."
+    )
+
+    await bot.send_message(message.chat.id, "👋", reply_markup=reply_kb)
+    await bot.send_message(message.chat.id, text, reply_markup=inline_kb)
+
 @dp.message_handler()
 async def main_handler(message: types.Message):
     text = message.text
@@ -25,21 +43,3 @@ async def main_handler(message: types.Message):
             "⚠️ Вы упускаете возможность быть в курсе всех новостей и акций!"
         )
         return await message.answer(text)
-
-@dp.message_handler(commands=["start"])
-async def start_message(message: types.Message):
-    await message.delete()
-    reply_kb, inline_kb = await get_start_keyboards()
-
-    is_subscribed = await check_subscribe(message.from_user.id)
-    if not is_subscribed:
-        inline_kb = await subscribe_kb()
-
-    text = (
-        "С помощью данного бота Вы сможете обезопасить свой игровой аккаунт от взлома и восстановить его в случае утраты пароля."
-        "Для привязки игрового аккаунта, воспользуйтесь кнопкой «Получить код»"
-        f"Перед началом взаимодействия, необходимо подписаться на наш новостной канал @{os.getenv("channel_username")}."
-    )
-
-    await bot.send_message(message.chat.id, "👋", reply_markup=reply_kb)
-    await bot.send_message(message.chat.id, text, reply_markup=inline_kb)
