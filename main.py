85fcd184349c1eaa2891c43f2ca38e31bd62636f
b1atov <barigalovec@gmail.com>
2026-07-30 13:47:39 +0300

    feat(main): add parse mode for bot

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import os
 
 load_dotenv()
 
-bot = Bot(token=os.getenv("bot_token"))
+bot = Bot(token=os.getenv("bot_token"), parse_mode="HTML")
 dp = Dispatcher(bot)
 
 if __name__ == "__main__":
+    from handlers import dp
     executor.start_polling(dp, skip_updates=True)
 
