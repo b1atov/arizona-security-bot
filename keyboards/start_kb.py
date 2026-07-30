aaa0271767f311c387a98304a28d382483a7bfc8
b1atov <barigalovec@gmail.com>
2026-07-30 14:26:10 +0300

    fix(keyboards): fix reply keyboard in the start command

--- keyboards/start_kb.py
+++ keyboards/start_kb.py
@@ -3,7 +3,7 @@ from aiogram import types
 async def get_start_keyboards():
     reply_kb = types.ReplyKeyboardMarkup(keyboard=[
         [types.KeyboardButton("Начать")],
-        [types.KeyboardButton("Узнать ID профиля"), types.KeyboardButton("Дополнительные рассылки"), types.KeyboardButton("Дополнительные рассылки")]
+        [types.KeyboardButton("Узнать ID профиля"), types.KeyboardButton("Дополнительные рассылки")]
     ])
     inline_kb = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text="Получить код", callback_data="get_code"))
 
