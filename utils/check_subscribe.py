7df61e2b6bbda14a8cd619bb829c60d82f2a2c86
b1atov <barigalovec@gmail.com>
2026-08-04 00:01:01 +0300

    fix(subscribe): treat restricted as subscribed and log errors

--- utils/check_subscribe.py
+++ utils/check_subscribe.py
@@ -1,10 +1,11 @@
-from aiogram import types
 from main import bot
 import os
+import logging
 
 async def check_subscribe(userId) -> bool:
     try: 
         member = await bot.get_chat_member(chat_id=int(os.getenv("channel_id")), user_id=userId)
-        return member.status in ["creator", "administrator", "member"]
+        return member.status in ["creator", "administrator", "member", "restricted"]
     except Exception:
+        logging.exception("check_subscribe failed")
         return False
